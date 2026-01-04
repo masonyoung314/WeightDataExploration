de1a7a265737b175841da81f60d651a68ec8d956
Mason Young <masonyou@umich.edu>
2026-01-04 11:35:59 +0000

    Changed wd

--- weights.R
+++ weights.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggplot2)
 library(lubridate)
 library(shiny)
-setwd("/Users/masonyoung/Desktop/Personal_Projects/WeightsOverTime")
+setwd("/Users/masonyoung/Desktop/Personal_Projects/WeightDataExploration")
 weights <- read.csv("weights.csv", header = TRUE)
 
 head(weights)
